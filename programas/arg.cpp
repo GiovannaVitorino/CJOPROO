ba67f91fecbc0bf26d15df397e50760f769efe2e
GiovannaVitorino <vitorinogiovanna@gmail.com>
2026-05-18 21:19:19 -0300

    correção

--- programas/arg.cpp
+++ programas/arg.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
-#include <vector>
-
 using namespace std;
 
-int main(int argc, char *arg[]){
+int main(int argc, char *argv[]) {
     cout << "Exemplo de argumentos de uma funcao principal\n\n";
-    cout << "Numero s de argumentis:\n\n"; << argc << "\n\n";
+    cout << "Numero de argumentos: " << argc << "\n\n";
 
-    for (int I = 0; i < argc; i++){
-        cout << "argumento " << i << ":"<< argv[i];
+    for (int i = 0; i < argc; i++) {
+        cout << "Argumento " << i << ": " << argv[i] << "\n";
     }
 
-return 0;
+    return 0;
 }
-
